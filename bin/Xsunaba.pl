a96e54dfd9cb69a78793f020fd63e9a3fd55f564
David Uhden Collado <david@uhden.dev>
2026-01-27 20:45:54 +0100

    Simplify comments

--- bin/Xsunaba.pl
+++ bin/Xsunaba.pl
@@ -1,33 +1,15 @@
 #!/usr/bin/perl
 
-# Xsunaba - Tool for sandboxing X11 applications
+# Tool for sandboxing X11 applications
 #
-# Originally based on http://blog.sleeplessbeastie.eu/2013/07/19/how-to-create-browser-sandbox/
-
-# MIT License
+# Uses Xephyr to create a nested X server and runs
+# the specified application inside it, with authentication
+# cookies managed via xauth.
+# Usage:
+#   Xsunaba.pl [application and args]
 #
-# Copyright (c) 2013 Milosz Galazka
-# Copyright (c) 2020 Morgan Aldridge
-# Copyright (c) 2025 David Uhden Collado
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+# See the LICENSE file at the top of the project tree for copyright
+# and license details.
 
 use strict;
 use warnings;
